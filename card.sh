a946d79152b1448181512d8e0afbd17273babb43
Joe Ruether <jrruethe@gmail.com>
2016-05-10 20:27:50 -0400

    Finished card script

--- card.sh
+++ card.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+if [ "$#" -ne 2 ]; then
+   echo "Usage: card.sh <type> <description>"
+   exit 1
+fi
+
+TYPE=$1
+DESCRIPTION=$2
+
 urlencode() {
   local length="${#1}"
   for (( i = 0; i < length; i++ )); do
@@ -13,15 +21,14 @@ urlencode() {
   done
 }
 
-TITLE=$(urlencode "Take control of your Twitter feed and make your posts stand out")
+TITLE=$(urlencode "${DESCRIPTION}")
 AVATAR=$(urlencode "http://jrruethe.github.io/blog/twitter-card/avatar.png")
 TWITTER=$(urlencode "@jrruethe")
-STICKER=$(urlencode "stickers/docker.png")
-BACKGROUND=$(urlencode "#ccc url(https://source.unsplash.com/featured/1200x630/?mountains) top left no-repeat")
+STICKER=$(urlencode "stickers/${TYPE}.png")
+BACKGROUND=$(urlencode "#ccc url(https://source.unsplash.com/featured/1200x630/?water) top left no-repeat")
 
 URL="http://jrruethe.github.io/blog/twitter-card/index.html?title=$TITLE&avatar=$AVATAR&twitter=$TWITTER&sticker=$STICKER&background=$BACKGROUND"
 
 docker run --rm -v `pwd`:/mnt -w /mnt docker-twitter-card -- webshot --window-size=1200/630 --render-delay=1000 $URL twitter-card.png
-# docker run --rm -v `pwd`:/mnt -w /mnt titpetric/webshot-cli --window-size=1200/630 --render-delay=2000 $URL twitter-card.png
-# docker run --rm -v `pwd`:/mnt -w /mnt docker-twitter-card -- /root/optipng -o7 twitter-card.png
+docker run --rm -v `pwd`:/mnt -w /mnt docker-twitter-card -- /root/optipng -o7 twitter-card.png
 
